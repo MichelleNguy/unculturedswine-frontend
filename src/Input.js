41a7817c779c14d430273f7c145fd2b1359bb917
Martin Crabtree <martin.crabtree@gmail.com>
2019-10-21 13:10:34 -0400

    displays checkboxes, but need to rethink events

--- src/Input.js
+++ src/Input.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './App.css';
 
 export default class Input extends Component {
+    
+    state={  }
+
+    displayOptions=()=>{
+        let eventdata = this.props.gamedata.events
+        if (eventdata  !== undefined ){
+            return eventdata.map((event)=>{
+                return(
+                    <label> 
+                        <input type="checkbox" />
+                        {event.title}
+                        <div > {event.description}  </div>
+                    </label>
+                )  
+            })
+        }
+        else{
+            return
+        }
+    }
+
+
+
     render() {
         return (
-            <div>
-                hello from input
-            </div>
+            <section>
+                <h2> Choose your family's destiny:  </h2>
+                <form >
+                    { this.displayOptions()}
+                </form>
+                
+                <div >This is where stuff goes</div>
+            </section>
         )
     }
 }
