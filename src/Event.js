9f0e204e9a81ee1000ed8481d72cd9ca2d103c34
Martin Crabtree <martin.crabtree@gmail.com>
2019-10-21 16:50:40 -0400

    checkboxes yo

---
+++ src/Event.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const Event=(props)=>{
+
+    return(
+        <label> :
+            <input name="temp" type="checkbox" checked="" onChange={null}  />
+        </label>
+    )
+    
+}
+
+export default Event
+
