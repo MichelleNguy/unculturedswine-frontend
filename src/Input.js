7a7539477e4d70bd0903dab5870ae579e10c79d9
Martin Crabtree <martin.crabtree@gmail.com>
2019-10-23 09:41:20 -0400

    some small changes to input

--- src/Input.js
+++ src/Input.js
@@ -12,8 +12,7 @@ export default class Input extends Component {
         event5: false,
      }
 
-     handleCheck=(event)=>{
-        
+     handleCheck=(event)=>{ 
         this.setState({
             [event.target.name]:  !!this.state[event.target.name] ? false  : true
         })
@@ -33,25 +32,6 @@ export default class Input extends Component {
                 <h2> Choose your family's destiny:  </h2>
                 <form >
                     { this.showEvents() }
-
-                    <label> :
-                        <input name="event1" type="checkbox" checked={this.state.event1} onChange={this.handleCheck}  />
-                    </label>
-                    <label>Option 2:
-                        <input name="event2" type="checkbox" checked={this.state.event2} onChange={this.handleCheck}   />
-                    </label>
-                    <label> Option 3:
-                        <input name="event3" type="checkbox" checked={this.state.event3} onChange={this.handleCheck}  />
-                    </label>
-                    <label> Option 4:
-                        <input name="event4" type="checkbox" checked={this.state.event4} onChange={this.handleCheck}   />
-                    </label>
-                    <label>Option 5:
-                        <input name="event5" type="checkbox"  checked={this.state.event5} onChange={this.handleCheck}  />
-                    </label>
-
-
-
                 </form>
                 
                 <div >This is where stuff goes</div>
